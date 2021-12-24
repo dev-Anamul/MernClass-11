37582c3c36179b615849ed2349355d81ccbfb7a0
dev-Anamul <ah9737677@gmail.com>
2021-12-24 23:12:33 +0600

    add marks student info

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable no-undef */
 /// //// developer section render functionality
 
@@ -5,7 +6,12 @@
 const devWrapper = document.querySelector('.developer__card__section');
 const studentWrapper = document.querySelector('.student__info__item');
 const tableBody = document.querySelector('.table > tbody');
+tableBody.id = 'thisis';
 console.log(tableBody);
+
+const studentDiv = document.querySelector('.students__card__wrapper');
+console.log(studentDiv);
+
 /// //// loop function here
 
 /// /////////// show developer data funciton here
@@ -83,4 +89,62 @@ family.map((fdata, i) => {
         <td><img src='${fdata.img} />'</td>
       </tr>`;
     return false;
+});
+
+/// ////// second student card elements
+
+student.forEach((data) => {
+    studentDiv.innerHTML += ` <div class="students__card">
+  <div class="front__side">
+    <img
+      src="${data.img}"
+      alt="Studetns Image"
+    />
+    <div class="students__details">
+      <h1>${data.Name}</h1>
+      <h2>Age &nbsp; : &nbsp; ${data.age}</h2>
+      <h2>Gender &nbsp; : &nbsp; ${data.Gender}</h2>
+      <h2>Location &nbsp; : &nbsp; ${data.Loc}</h2>
+    </div>
+  </div>
+  <div class="back__side">
+    <table class="table mark__table" border="1">
+      <caption>
+        Student Marks
+      </caption>
+      <thead>
+        <tr>
+          <th>Subject</th>
+          <th>Marks</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Bangla</td>
+          <td>${data.marks.Bangla}</td>
+        </tr>
+        <tr>
+          <td>English</td>
+          <td>${data.marks.English}</td>
+        </tr>
+        <tr>
+          <td>Math</td>
+          <td>${data.marks.math}</td>
+        </tr>
+        <tr>
+          <td>Science</td>
+          <td>${data.marks.Science}</td>
+        </tr>
+        <tr>
+          <td>Accounting</td>
+          <td>${data.marks.Accounting}</td>
+        </tr>
+        <tr>
+          <td>Religion</td>
+          <td>${data.marks.Religion}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>`;
 });
